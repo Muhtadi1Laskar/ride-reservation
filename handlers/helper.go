51675188abb02fe2887bceacfa3998dab893f91e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-19 02:07:52 +0600

    first commit

---
+++ handlers/helper.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func ReadRequestBody(r *http.Request, target interface{}) error {
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read request body: %v", err)
+	}
+	defer r.Body.Close()
+
+	if err := json.Unmarshal(reqBody, target); err != nil {
+		return fmt.Errorf("unable to parse the JSON body: %v", err)
+	}
+
+	validate := validator.New()
+	err = validate.Struct(target)
+	if err != nil {
+		return fmt.Errorf("validation error: %w", err)
+	}
+
+	return nil
+}
+
+func WriteJSONResponse(w http.ResponseWriter, statusCode int, response interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+}
+
+func WriteError(w http.ResponseWriter, statusCode int, err string) {
+	WriteJSONResponse(w, statusCode, ErrorResponse{ Error: err })
+}
